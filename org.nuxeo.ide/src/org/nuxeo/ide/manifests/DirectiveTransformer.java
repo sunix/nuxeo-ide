47dab4e40f3091366326235350af19db45fc76d0
Stephane Lacoin <slacoin@nuxeo.com>
2010-07-18 08:52:01 +0200

    APG-240: integrate with remote vcs (NXP-5053) https://jira.nuxeo.org/browse/APG-240
    integrated unit tests in implementation code

--- org.nuxeo.ide/src/org/nuxeo/ide/manifests/DirectiveTransformer.java
+++ org.nuxeo.ide/src/org/nuxeo/ide/manifests/DirectiveTransformer.java
@@ -3,152 +3,171 @@
  */
 package org.nuxeo.ide.manifests;
 
+import static org.junit.Assert.assertEquals;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
+import org.junit.Test;
 
 public abstract class DirectiveTransformer {
 
-	static final Log log = LogFactory.getLog(DirectiveTransformer.class);
+    static final Log log = LogFactory.getLog(DirectiveTransformer.class);
 
-	protected DirectiveTransformer next;
+    protected DirectiveTransformer next;
 
-	public DirectiveTransformer link(DirectiveTransformer next) {
-		return this.next = next;
-	}
+    public DirectiveTransformer link(DirectiveTransformer next) {
+        return this.next = next;
+    }
 
-	protected DirectiveInfo invokeNext(DirectiveInfo info) throws DirectiveTransformerVeto {
-		if (next == null) {
-			return info;
-		}
-		return next.process(info);
-	}
+    protected DirectiveInfo invokeNext(DirectiveInfo info) throws DirectiveTransformerVeto {
+        if (next == null) {
+            return info;
+        }
+        return next.process(info);
+    }
 
-	protected abstract DirectiveInfo process(DirectiveInfo info) throws DirectiveTransformerVeto;
+    protected abstract DirectiveInfo process(DirectiveInfo info) throws DirectiveTransformerVeto;
 
-	public static DirectiveTransformer noop() {
-		return new Noop();
-	}
+    public static DirectiveTransformer noop() {
+        return new Noop();
+    }
 
-	public static class Noop extends DirectiveTransformer {
+    public static class Noop extends DirectiveTransformer {
 
-		@Override
-		public DirectiveInfo process(DirectiveInfo info)
-				throws DirectiveTransformerVeto {
-			return invokeNext(info);
-		}
+        @Override
+        public DirectiveInfo process(DirectiveInfo info) throws DirectiveTransformerVeto {
+            return invokeNext(info);
+        }
 
-	}
+    }
 
-	public static DirectiveTransformer multiValuesPrettyPrinter() {
-		return new MultiValuesPrettyPrinter();
-	}
+    public static DirectiveTransformer multiValuesPrettyPrinter() {
+        return new MultiValuesPrettyPrinter();
+    }
 
-	static class MultiValuesPrettyPrinter extends DirectiveTransformer {
+    static class MultiValuesPrettyPrinter extends DirectiveTransformer {
 
-		public static final String LS = System.getProperty("line.separator");
+        public static final String LS = System.getProperty("line.separator");
 
-		@Override
-		public DirectiveInfo process(DirectiveInfo info)
-				throws DirectiveTransformerVeto {
-			String hr = info.value.replaceAll(",", ","+LS+" ");
-			if (!hr.startsWith(" ")) {
-				hr = " " + hr;
-			}
-			return invokeNext(new DirectiveInfo(info.name, hr));
-		}
+        @Override
+        public DirectiveInfo process(DirectiveInfo info) throws DirectiveTransformerVeto {
+            String hr = info.value.replaceAll(",", "," + LS + " ");
+            if (!hr.startsWith(" ")) {
+                hr = " " + hr;
+            }
+            return invokeNext(new DirectiveInfo(info.name, hr));
+        }
 
-	}
+    }
 
-	public static DirectiveTransformer nameVeto(String name) {
-		return new NameVeto(name);
-	}
+    @Test
+    public void testMultiValues() throws DirectiveTransformerVeto {
+        DirectiveTransformer transf = new DirectiveTransformer.MultiValuesPrettyPrinter();
+        DirectiveInfo info = transf.process(new DirectiveInfo("test", "toto,titi"));
+        assertEquals("name is altered", "test", info.name);
+        assertEquals("line is not pretty printed", " toto,\n titi", info.value);
+    }
 
-	static class NameVeto extends DirectiveTransformer {
+    public static DirectiveTransformer nameVeto(String name) {
+        return new NameVeto(name);
+    }
 
-		public NameVeto(String name) {
-			this.name = name;
-		}
-		String name;
+    static class NameVeto extends DirectiveTransformer {
 
-		@Override
-		public DirectiveInfo process(DirectiveInfo info)
-				throws DirectiveTransformerVeto {
-			if (name.equals(info.name)) {
-				throw new DirectiveTransformerVeto(this, info);
-			}
-			return invokeNext(info);
-		}
-	}
+        public NameVeto(String name) {
+            this.name = name;
+        }
 
-	public static DirectiveTransformer logger() {
-		return new Logger();
-	}
+        String name;
 
-	static class Logger extends DirectiveTransformer {
+        @Override
+        public DirectiveInfo process(DirectiveInfo info) throws DirectiveTransformerVeto {
+            if (name.equals(info.name)) {
+                throw new DirectiveTransformerVeto(this, info);
+            }
+            return invokeNext(info);
+        }
+    }
 
-		public DirectiveInfo process(DirectiveInfo source) throws DirectiveTransformerVeto {
-			try {
-				DirectiveInfo transformed = invokeNext(source);
-				log.info(source + " -> " + transformed);
-				return transformed;
-			} catch (DirectiveTransformerVeto veto) {
-				log.info("veto on " + veto.info);
-				throw veto;
-			}
+    @Test(expected = DirectiveTransformerVeto.class)
+    public void testNameVeto() throws DirectiveTransformerVeto {
+        DirectiveTransformer nameVeto = new DirectiveTransformer.NameVeto("pfff");
+        nameVeto.process(new DirectiveInfo("pfff", ""));
+    }
 
-		}
-	}
+    public static DirectiveTransformer logger() {
+        return new Logger();
+    }
 
-	public static DirectiveTransformer exportPackagesVersionEraser(String label) {
-		return new ExportPackageVersionEraser(label);
-	}
+    static class Logger extends DirectiveTransformer {
 
-	public static class ExportPackageVersionEraser extends DirectiveTransformer {
+        public DirectiveInfo process(DirectiveInfo source) throws DirectiveTransformerVeto {
+            try {
+                DirectiveInfo transformed = invokeNext(source);
+                log.info(source + " -> " + transformed);
+                return transformed;
+            } catch (DirectiveTransformerVeto veto) {
+                log.info("veto on " + veto.info);
+                throw veto;
+            }
 
-		String label;
-		String expression;
+        }
+    }
 
-		public ExportPackageVersionEraser(String label) {
-			this.label = label;
-			this.expression = String.format(";version=\"%s\"", label);
-		}
+    public static DirectiveTransformer exportPackagesVersionEraser(String label) {
+        return new ExportPackageVersionEraser(label);
+    }
 
-		@Override
-		protected DirectiveInfo process(DirectiveInfo info)
-				throws DirectiveTransformerVeto {
-			if (!"Export-Package".equals(info.name)) {
-				return invokeNext(info);
-			}
-			DirectiveInfo transformed = invokeNext(info);
-			return new DirectiveInfo(transformed.name,
-					transformed.value.replaceAll(expression, ""));
-		}
+    public static class ExportPackageVersionEraser extends DirectiveTransformer {
 
+        String label;
 
-	}
+        String expression;
 
-	public static DirectiveTransformer bundleVersionOverwriter(String label) {
-		return new BundleVersionOverwriter(label);
-	}
+        public ExportPackageVersionEraser(String label) {
+            this.label = label;
+            this.expression = String.format(";version=\"%s\"", label);
+        }
 
-	public static class BundleVersionOverwriter extends DirectiveTransformer {
+        @Override
+        protected DirectiveInfo process(DirectiveInfo info) throws DirectiveTransformerVeto {
+            if (!"Export-Package".equals(info.name)) {
+                return invokeNext(info);
+            }
+            DirectiveInfo transformed = invokeNext(info);
+            return new DirectiveInfo(transformed.name, transformed.value.replaceAll(expression, ""));
+        }
 
-		final String label;
+    }
 
-		public BundleVersionOverwriter(String label) {
-			this.label = label;
-		}
+    @Test
+    public void testExportPackagesVersionEraser() throws DirectiveTransformerVeto {
+        ExportPackageVersionEraser eraser = new ExportPackageVersionEraser("1.6.2.SNAPSHOT");
+        DirectiveInfo original = new DirectiveInfo("Export-Package", " org.nuxeo;version=\"1.6.2.SNAPSHOT\",\n org.nuxeo.other;version=\"1.6.2.SNAPSHOT\"\n");
+        DirectiveInfo info = eraser.process(original);
+        assertEquals("incorrect packages", " org.nuxeo,\n org.nuxeo.other\n", info.value);
+    }
 
-		@Override
-		protected DirectiveInfo process(DirectiveInfo info)
-				throws DirectiveTransformerVeto {
-			if ("Bundle-Version".equals(info.name)) {
-				info = new DirectiveInfo(info.name, label);
-			}
-			return invokeNext(info);
-		}
+    public static DirectiveTransformer bundleVersionOverwriter(String label) {
+        return new BundleVersionOverwriter(label);
+    }
 
-	}
+    public static class BundleVersionOverwriter extends DirectiveTransformer {
+
+        final String label;
+
+        public BundleVersionOverwriter(String label) {
+            this.label = label;
+        }
+
+        @Override
+        protected DirectiveInfo process(DirectiveInfo info) throws DirectiveTransformerVeto {
+            if ("Bundle-Version".equals(info.name)) {
+                info = new DirectiveInfo(info.name, label);
+            }
+            return invokeNext(info);
+        }
+
+    }
 
 }
--- org.nuxeo.ide/src/org/nuxeo/ide/manifests/ManifestFormater.java
+++ org.nuxeo.ide/src/org/nuxeo/ide/manifests/ManifestFormater.java
@@ -1,11 +1,15 @@
 package org.nuxeo.ide.manifests;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URL;
 import java.util.jar.Manifest;
+
+import org.junit.Test;
 
 public class ManifestFormater {
 
@@ -24,11 +28,19 @@ public class ManifestFormater {
 		Manifest mf = new Manifest(new FileInputStream(file));
 		format(mf, new FileOutputStream(file));
 	}
-	
+
 	public void format(Manifest mf, OutputStream out) throws IOException {
 		ManifestWriter writer = new ManifestWriter(out);
 		configureWriter(writer);
 		writer.write(mf);
+	}
+
+	@Test
+	public void writeBinary() throws IOException {
+		URL mfLocation = ManifestFormater.class.getResource("test-MANIFEST.mf");
+		Manifest mf = new Manifest(mfLocation.openStream());
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		new ManifestFormater().format(mf, os);
 	}
 
 }
