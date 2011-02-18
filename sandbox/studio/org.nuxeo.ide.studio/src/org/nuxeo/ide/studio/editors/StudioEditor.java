a447c9a522c2fe4683520f9bc06829948a2a8948
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2011-02-18 17:01:26 +0100

    fixed set part name

--- sandbox/studio/org.nuxeo.ide.studio/src/org/nuxeo/ide/studio/editors/StudioEditor.java
+++ sandbox/studio/org.nuxeo.ide.studio/src/org/nuxeo/ide/studio/editors/StudioEditor.java
@@ -99,6 +99,7 @@ public class StudioEditor extends EditorPart {
                             Node node = studioBrowserView.selectNode(id);
                             if ( node != null ){
                                 setInputWithNotify(new StudioEditorInput(node));
+                                setPartName("todo");
                             }
                         }
 
