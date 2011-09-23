d1c1be4ac88abbd6236c4918efac6356da8ea617
Stephane Lacoin <slacoin@nuxeo.com>
2011-09-23 16:04:59 +0200

    Backed out changeset: d369b70a8175

--- plugins/org.nuxeo.ide.sdk/src/org/nuxeo/ide/sdk/SDKInfo.java
+++ plugins/org.nuxeo.ide.sdk/src/org/nuxeo/ide/sdk/SDKInfo.java
@@ -25,7 +25,6 @@ import java.net.URLEncoder;
 import java.util.List;
 import java.util.Properties;
 
-import org.apache.commons.exec.CommandLine;
 import org.nuxeo.ide.common.IOUtils;
 import org.nuxeo.ide.common.UI;
 import org.nuxeo.ide.sdk.server.VMUtils;
@@ -140,17 +139,20 @@ public class SDKInfo {
         IOUtils.writeLines(sdkConf, lines);
     }
 
-    public static CommandLine newCommandLine(File installRoot, String command) {
-        CommandLine cmd = new CommandLine(VMUtils.getJavaExecutablePath());
-        cmd.addArgument("-Dnuxeo.home=" + installRoot.getAbsolutePath());
-        cmd.addArgument("-Dnuxeo.conf="
-                + new File(installRoot, "bin/nuxeo-sdk.conf").getAbsolutePath());
-        cmd.addArgument("-jar");
-        cmd.addArgument(new File(installRoot, "bin/nuxeo-launcher.jar").getAbsolutePath());
+    public static ProcessBuilder newProcessBuilder(File installRoot,
+            String command) {
+        ProcessBuilder builder = new ProcessBuilder(
+                VMUtils.getJavaExecutablePath(),
+                "-Dnuxeo.home=" + installRoot.getAbsolutePath(),
+                "-Dnuxeo.conf="
+                        + new File(installRoot, "bin/nuxeo-sdk.conf").getAbsolutePath(),
+                "-jar",
+                new File(installRoot, "bin/nuxeo-launcher.jar").getAbsolutePath());
         if (command != null) {
-            cmd.addArgument(command);
+            builder.command().add(command);
         }
-        return cmd;
+        builder.directory(new File(installRoot, "bin"));
+        return builder;
     }
 
     public static SDKInfo loadSDK(File dir) throws IOException {
