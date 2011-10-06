47f77c10a39d97f1452b999ae6ab814fd1636c15
Stephane Lacoin <slacoin@nuxeo.com>
2011-10-06 17:47:02 +0200

    NXIDE-47 fixed resource path setting (missing classes)

---
+++ plugins/org.nuxeo.ide.sdk/src/org/nuxeo/ide/sdk/templates/cmd/ResourcePathCommand.java
@@ -0,0 +1,49 @@
+/*
+ * (C) Copyright 2006-2011 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     slacoin
+ */
+package org.nuxeo.ide.sdk.templates.cmd;
+
+import java.io.File;
+
+import org.nuxeo.ide.sdk.model.PomModel;
+import org.nuxeo.ide.sdk.templates.TemplateContext;
+import org.osgi.framework.Bundle;
+import org.w3c.dom.Element;
+
+/**
+ * Add new source folder to project
+ * 
+ * 
+ */
+public class ResourcePathCommand implements Command {
+
+    protected String path;
+    
+    @Override
+    public void init(Element element) {
+        path = element.getAttribute("path");
+    }
+
+    @Override
+    public void execute(TemplateContext ctx, Bundle bundle, File projectDir)
+            throws Exception {
+              File pomFile = new File(projectDir, "pom.xml");
+        PomModel pom = new PomModel(pomFile);
+        pom.addBuildHelperResource(path);
+        pom.write(pomFile);  
+    }
+
+}
